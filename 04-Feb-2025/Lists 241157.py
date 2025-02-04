5a09623d1f6d323109208b65f07ba4457c61c162
Beteab Admassu <82194935+BeteabAdmassu@users.noreply.github.com>
2025-02-04 21:55:55 +0300

    Created 'Lists' by A2SV Hub

---
+++ 04-Feb-2025/Lists 241157.py
@@ -0,0 +1,26 @@
+# Problem: Lists - https://www.hackerrank.com/challenges/python-lists/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    N = int(input())
+    
+    arr = []
+    
+    for _ in range(N):
+        commands = input()
+        com = commands.split(" ")
+        
+        if com[0] == "insert":
+            arr.insert(int(com[1]),int(com[2]))
+        elif com[0] == "print":
+            print(arr)
+        elif com[0] == "remove":
+            arr.remove(int(com[1]))
+        elif com[0] == "append":
+            arr.append(int(com[1]))
+        elif com[0] == "sort":
+            arr.sort()
+        elif com[0] == "pop":
+            arr.pop()
+        elif com[0] == "reverse":
+            arr.reverse()
+            
