c06e6c0ae91845aca4d80094ca3e2d70770ac1c5
Beteab Admassu <82194935+BeteabAdmassu@users.noreply.github.com>
2025-02-05 07:07:48 +0300

    Created 'Presents' by A2SV Hub

---
+++ 05-Feb-2025/Presents 241895.py
@@ -0,0 +1,15 @@
+# Problem: Presents - https://codeforces.com/problemset/problem/136/A
+
+import copy
+N = int(input())
+array = [0] * N
+
+arr = input().split(" ")
+t = arr.copy()
+
+for i in range(len(arr)):
+    arr[int(t[i]) - 1] = i + 1
+
+output = " ".join(map(str,arr))
+
+print(output)
