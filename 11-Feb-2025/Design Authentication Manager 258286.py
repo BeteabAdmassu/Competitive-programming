02954b409fa876860eea05126bd64a2f58667728
Beteab Admassu <82194935+BeteabAdmassu@users.noreply.github.com>
2025-02-11 23:10:48 +0300

    Created 'Design Authentication Manager' by A2SV Hub

---
+++ 11-Feb-2025/Design Authentication Manager 258286.py
@@ -0,0 +1,30 @@
+# Problem: Design Authentication Manager - https://leetcode.com/problems/design-authentication-manager/
+
+class AuthenticationManager:
+
+    def __init__(self, timeToLive: int):
+        self.timeToLive = timeToLive
+        self.tokens = {}
+        
+
+    def generate(self, tokenId: str, currentTime: int) -> None:
+        self.tokens[tokenId] = currentTime + self.timeToLive
+        
+
+    def renew(self, tokenId: str, currentTime: int) -> None:
+        if tokenId in self.tokens and self.tokens[tokenId] > currentTime:
+            self.tokens[tokenId] = currentTime + self.timeToLive 
+
+        
+
+    def countUnexpiredTokens(self, currentTime: int) -> int:
+        self.tokens = {token: expiry for token, expiry in self.tokens.items() if expiry > currentTime}  
+        return len(self.tokens)
+        
+
+
+# Your AuthenticationManager object will be instantiated and called as such:
+# obj = AuthenticationManager(timeToLive)
+# obj.generate(tokenId,currentTime)
+# obj.renew(tokenId,currentTime)
+# param_3 = obj.countUnexpiredTokens(currentTime)
