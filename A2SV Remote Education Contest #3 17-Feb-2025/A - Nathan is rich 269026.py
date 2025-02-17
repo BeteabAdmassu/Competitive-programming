24d3cc53bed4d3210f9c1d1e3895b34bbd943d07
Beteab Admassu <82194935+BeteabAdmassu@users.noreply.github.com>
2025-02-17 15:24:50 +0300

    Created 'A - Nathan is rich' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 17-Feb-2025/A - Nathan is rich 269026.py
@@ -0,0 +1,15 @@
+# Problem: A - Nathan is rich - https://codeforces.com/gym/588094/problem/A
+
+N = int(input())
+
+for _ in range(N):
+    _input = int(input())
+    if _input ==2:
+        print("1")
+    else:
+        q = _input // 4
+        r = _input % 4
+
+        if r != 0:
+            q += 1
+        print(q)
