1e0510d06a64310eae1e2ee08424d4ab1fb11c20
Beteab Admassu <82194935+BeteabAdmassu@users.noreply.github.com>
2025-02-17 17:06:09 +0300

    Created 'B - Card Game' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 17-Feb-2025/B - Card Game 269154.py
@@ -0,0 +1,14 @@
+# Problem: B - Card Game - https://codeforces.com/gym/588094/problem/B
+
+n = int(input())
+cards = list(map(int, input().split()))
+
+
+indexed_cards = [(cards[i], i + 1) for i in range(n)]
+
+
+indexed_cards.sort()
+
+
+for i in range(n // 2):
+    print(indexed_cards[i][1], indexed_cards[n - 1 - i][1])
