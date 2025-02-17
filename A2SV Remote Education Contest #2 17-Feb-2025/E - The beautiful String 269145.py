5e79db3448c36dd468a20fa1e4155d400a13564e
Beteab Admassu <82194935+BeteabAdmassu@users.noreply.github.com>
2025-02-17 16:58:54 +0300

    Created 'E - The beautiful String' by A2SV Hub

---
+++ A2SV Remote Education Contest #2 17-Feb-2025/E - The beautiful String 269145.py
@@ -0,0 +1,38 @@
+# Problem: E - The beautiful String - https://codeforces.com/gym/586622/problem/E
+
+
+t = int(input().strip())
+
+
+for _ in range(t):
+    s = list(input().strip())
+    q = int(input())
+    hash_check = set()
+    
+    
+
+    for i in range(len(s)):
+        if s[i:i+4] == ["1","1","0","0"]:
+            hash_check.add(i)
+    
+
+ 
+    for _ in range(q):
+        i, v = map(int, input().split())
+        i -= 1  
+
+        s[i] = str(v)
+
+        for j in range(max(0,i-3),min(i+1, len(s))):
+            if s[j:j+4] == ["1", "1", "0", "0"]:
+                hash_check.add(j)
+            else:
+                hash_check.discard(j)
+
+
+        if len(hash_check) > 0:
+            print("YES")
+        else:
+            print("NO")
+
+
