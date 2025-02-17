c99912597530baf4aa4a140917b77bb4407df559
Beteab Admassu <82194935+BeteabAdmassu@users.noreply.github.com>
2025-02-17 10:01:05 +0300

    Created 'Majority Element II (Optional)' by A2SV Hub

---
+++ 17-Feb-2025/Majority Element II (Optional) 268191.py
@@ -0,0 +1,12 @@
+# Problem: Majority Element II (Optional) - https://leetcode.com/problems/majority-element-ii/
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        numshash = Counter(nums)
+        output = []
+        
+        for num in numshash:
+            if  numshash[num] > len(nums) / 3:
+                output.append(num)
+        return output
+        
