16ad9736ea8b3cd179448bc1348d60c185764379
Beteab Admassu <82194935+BeteabAdmassu@users.noreply.github.com>
2025-02-21 22:13:06 +0300

    Created 'Set Matrix Zeroes' by A2SV Hub

---
+++ 21-Feb-2025/Set Matrix Zeroes 279636.py
@@ -0,0 +1,29 @@
+# Problem: Set Matrix Zeroes - https://leetcode.com/problems/set-matrix-zeroes/
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+
+
+        col = set()
+        row = set()
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == 0:
+                    col.add(i)
+                    row.add(j)
+
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if i in col or j in row:
+                    matrix[i][j] = 0
+               
+
+
+        
+
+
+        
