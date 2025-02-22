73a7025fc6cb1a98367821766c996eea2c06d8b1
Beteab Admassu <82194935+BeteabAdmassu@users.noreply.github.com>
2025-02-22 22:16:08 +0300

    Created 'A. Beautiful Matrix' by A2SV Hub

---
+++ 22-Feb-2025/A. Beautiful Matrix 280500.py
@@ -0,0 +1,18 @@
+# Problem: A. Beautiful Matrix - https://codeforces.com/problemset/problem/263/A
+
+matrix = []
+x, y = 0, 0
+for i in range(5):
+    matrix.append(list(map(int, input().split())))
+    for j in range(5):
+        if matrix[i][j] == 1:
+            x, y = i, j
+if x > 2:
+    x = x - 2
+else: 
+    x = 2 - x
+if y > 2:
+    y = y -2
+else:
+    y = 2 - y
+print(x+y)
