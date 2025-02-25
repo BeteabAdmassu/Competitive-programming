d1eaa8533e77457c707d5df346f465beb22d9590
Beteab Admassu <82194935+BeteabAdmassu@users.noreply.github.com>
2025-02-25 09:38:44 +0300

    Created 'A - Harmonic Melody' by A2SV Hub

---
+++ A2SV Remote Contest #4 25-Feb-2025/A - Harmonic Melody 282220.py
@@ -0,0 +1,18 @@
+# Problem: A - Harmonic Melody - https://codeforces.com/gym/590201/problem/A
+
+N = int(input())
+
+
+for _ in range(N):
+    M = int(input())
+
+    notes =  list(map(int, input().split()))
+
+
+    perfect = True
+    for i in range(len(notes) - 1):
+        interval = abs(notes[i] - notes[i + 1])
+        if interval not in {5, 7}:
+            perfect = False
+            break
+    print("YES" if perfect else "NO")
