e46a8bb834251b9d67a84ca7bf37d1b73f264a9a
Beteab Admassu <82194935+BeteabAdmassu@users.noreply.github.com>
2025-02-25 09:38:53 +0300

    Created 'C - Happy Partitioning' by A2SV Hub

---
+++ A2SV Remote Contest #4 25-Feb-2025/C - Happy Partitioning 282221.py
@@ -0,0 +1,24 @@
+# Problem: C - Happy Partitioning - https://codeforces.com/gym/590201/problem/C
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = input().strip()
+
+    p0= [0]*(n+1)
+    p1 = [0]*(n+1)
+    for i in range(n):
+        p0[i+1] = p0[i] + (1 if a[i] == '0' else 0)
+        p1[i+1] = p1[i] + (1 if a[i] == '1' else 0)
+
+    valid_positions = []
+    for i in range(n+1):
+        left_count = i
+        right_count = n - i
+        need_left = (left_count + 1) // 2
+        need_right = (right_count + 1) // 2
+        if p0[i] >= need_left and (p1[n] - p1[i]) >= need_right:
+            valid_positions.append(i)
+
+    best_pos = min(valid_positions, key=lambda x: (abs(x - n / 2), x))
+    print(best_pos)
