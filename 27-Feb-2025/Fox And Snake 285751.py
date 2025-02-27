5c317011f2bf9ddf7cda6f5ddecfd6f45a3b1ab3
Beteab Admassu <82194935+BeteabAdmassu@users.noreply.github.com>
2025-02-27 10:16:04 +0300

    Created 'Fox And Snake' by A2SV Hub

---
+++ 27-Feb-2025/Fox And Snake 285751.py
@@ -0,0 +1,21 @@
+# Problem: Fox And Snake - https://codeforces.com/problemset/problem/510/A
+
+col,  row = map(int,input().split())
+
+rows = ['#'] * row
+snake = ['.'] * row
+snake[row - 1] = '#'
+snakeR = ['.'] * row
+snakeR[0] = '#'
+
+flag = False
+for i in range(col):
+    if i % 2 == 0:
+        print("".join(rows))
+    else:
+        if flag:
+            print("".join(snakeR))
+            flag = False
+        else:
+            print("".join(snake))
+            flag = True
