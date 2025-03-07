bfed20bf07d428085a1aae1aacc84acd4fe16be0
Beteab Admassu <82194935+BeteabAdmassu@users.noreply.github.com>
2025-03-07 09:36:11 +0300

    Created 'Less or Equal' by A2SV Hub

---
+++ 07-Mar-2025/Less or Equal 295558.py
@@ -0,0 +1,21 @@
+# Problem: Less or Equal - https://codeforces.com/problemset/problem/977/C
+
+n,k = input().split()
+n = int(n)
+k = int(k)
+
+
+array = list(map(int,input().split()))
+array.sort()
+
+
+
+if k == 0:
+    if array[0] == 1:
+        print(-1)
+    else:
+        print(1)
+elif k == n or array[k-1] != array[k]:
+    print(array[k-1])
+else:
+    print(-1)
