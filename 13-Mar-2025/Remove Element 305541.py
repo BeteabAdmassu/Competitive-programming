1f990b0ddefbf6db351bc70030b75187978a675c
Beteab Admassu <82194935+BeteabAdmassu@users.noreply.github.com>
2025-03-13 20:33:02 +0300

    Created 'Remove Element' by A2SV Hub

---
+++ 13-Mar-2025/Remove Element 305541.py
@@ -0,0 +1,15 @@
+# Problem: Remove Element - https://leetcode.com/problems/remove-element/description/
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        i = 0
+        for x in nums:
+            if x != val:
+                nums[i] = x
+                i += 1
+        return i
