9807031ea29a17874c1a7392b2a83925412f9c82
Beteab Admassu <82194935+BeteabAdmassu@users.noreply.github.com>
2025-03-20 22:51:05 +0300

    Created 'fruit-into-baskets' by A2SV Hub

---
+++ 20-Mar-2025/fruit-into-baskets 313748.py
@@ -0,0 +1,24 @@
+# Problem: fruit-into-baskets - https://leetcode.com/problems/fruit-into-baskets/
+
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        left = 0
+        maximum = 0
+        basket = {}
+        
+        for right in range(len(fruits)):
+            fruit = fruits[right]
+            
+            if fruit not in basket:
+                basket[fruit] = 0
+            basket[fruit] += 1
+            
+            while len(basket) > 2:
+                basket[fruits[left]] -= 1
+                if basket[fruits[left]] == 0:
+                    del basket[fruits[left]]
+                left += 1
+        
+            maximum = max(maximum, right - left + 1)
+        
+        return maximum
