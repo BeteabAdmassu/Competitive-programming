512fa3d844a49fa7c4614456756ae07d34752bea
Beteab Admassu <82194935+BeteabAdmassu@users.noreply.github.com>
2025-03-24 22:29:10 +0300

    Created 'C - The Quantum Canyon Conundrum' by A2SV Hub

---
+++ A2SV Remote Contest #7 24-Mar-2025/C - The Quantum Canyon Conundrum 317254.py
@@ -0,0 +1,20 @@
+# Problem: C - The Quantum Canyon Conundrum - https://codeforces.com/gym/596004/problem/C
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+
+    compressed = [arr[0]]
+    for x in arr[1:]:
+        if x != compressed[-1]:
+            compressed.append(x)
+    
+    c = 0
+    for i in range(len(compressed)):
+        left_greater = (i == 0) or (compressed[i-1] > compressed[i])
+        right_greater = (i == len(compressed) - 1) or (compressed[i+1] > compressed[i])
+        if left_greater and right_greater:
+            c += 1
+
+    print("YES" if c == 1 else "NO")
