7bf3a70f267a5e52beb0afbf2f9d8be181e18ef7
Beteab Admassu <82194935+BeteabAdmassu@users.noreply.github.com>
2025-03-28 18:41:26 +0300

    Created 'Maximum subarray' by A2SV Hub

---
+++ 28-Mar-2025/Maximum subarray 321003.py
@@ -0,0 +1,14 @@
+# Problem: Maximum subarray - https://leetcode.com/problems/maximum-subarray/
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_sub = nums[0]
+        cur_sub = 0
+
+        for n in nums:
+            if cur_sub < 0:
+                cur_sub = 0
+            cur_sub += n
+            max_sub = max(max_sub,cur_sub)
+        return max_sub
+            
