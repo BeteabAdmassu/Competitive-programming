a1cd3c46fee58466c06c1d755fc387d50c6d9d89
Beteab Admassu <82194935+BeteabAdmassu@users.noreply.github.com>
2025-03-29 17:03:07 +0300

    Created 'Continuous Subarray Sum' by A2SV Hub

---
+++ 29-Mar-2025/Continuous Subarray Sum 321552.py
@@ -0,0 +1,17 @@
+# Problem: Continuous Subarray Sum - https://leetcode.com/problems/continuous-subarray-sum/
+
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        reminder = {0 : -1}
+        total = 0
+
+
+        for i in range(len(nums)):
+            total += nums[i]
+            r = total % k
+
+            if r not in reminder:
+                reminder[r] = i
+            elif i - reminder[r] > 1:
+                return True
+        return False
