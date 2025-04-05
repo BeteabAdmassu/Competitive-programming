e2a35bed1f6bd616de214d25c59a3d66fdff822d
Beteab Admassu <82194935+BeteabAdmassu@users.noreply.github.com>
2025-04-05 22:19:06 +0300

    Created 'Count the Number of Good Subarrays' by A2SV Hub

---
+++ 05-Apr-2025/Count the Number of Good Subarrays 326981.py
@@ -0,0 +1,20 @@
+# Problem: Count the Number of Good Subarrays - https://leetcode.com/problems/count-the-number-of-good-subarrays/description/
+
+class Solution:
+    def countGood(self, nums: List[int], k: int) -> int:
+        goodSubarrays = 0
+        left = 0
+        freq = defaultdict(int)
+
+        for right in range(len(nums)):
+            k -= freq[nums[right]]
+            freq[nums[right]] += 1
+            while k <= 0:
+                freq[nums[left]] -= 1
+                k += freq[nums[left]]
+                left += 1
+
+            goodSubarrays += left
+
+        return goodSubarrays
+        
