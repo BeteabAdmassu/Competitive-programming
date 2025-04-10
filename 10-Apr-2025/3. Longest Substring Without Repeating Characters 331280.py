8a54ac0304432dab7f75360f85fc79e0d9a9e2d2
Beteab Admassu <82194935+BeteabAdmassu@users.noreply.github.com>
2025-04-10 22:27:08 +0300

    Created '3. Longest Substring Without Repeating Characters' by A2SV Hub

---
+++ 10-Apr-2025/3. Longest Substring Without Repeating Characters 331280.py
@@ -0,0 +1,29 @@
+# Problem: 3. Longest Substring Without Repeating Characters - https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+
+        longest_substring = 0 
+        counter = 0 
+
+        left, right = 0, 0
+        char_set = set()
+      
+
+        while right < len(s):
+
+            if s[right] not in char_set:
+                char_set.add(s[right])
+                counter += 1
+                longest_substring = max(longest_substring, counter)
+                right += 1
+
+            else:
+                char_set.clear()
+                right = left
+                left += 1
+                counter = 0
+    
+        return longest_substring
+
+
