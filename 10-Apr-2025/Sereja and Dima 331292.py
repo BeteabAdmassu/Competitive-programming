528c93e428eca697b5d8ee46c7af2b79d95aee22
Beteab Admassu <82194935+BeteabAdmassu@users.noreply.github.com>
2025-04-10 22:37:53 +0300

    Created 'Sereja and Dima' by A2SV Hub

---
+++ 10-Apr-2025/Sereja and Dima 331292.py
@@ -0,0 +1,26 @@
+# Problem: Sereja and Dima - https://codeforces.com/problemset/problem/381/A
+
+n = int(input())
+
+cards = list(map(int, input().split()))
+
+
+left, right = 0, n - 1
+sereja = 0
+dima = 0
+for i in range(n):
+    if i % 2 == 0:
+        if cards[left] > cards[right]:
+            sereja += cards[left]
+            left += 1
+        else:
+            sereja += cards[right]
+            right -= 1
+    else:
+        if cards[left] > cards[right]:
+            dima += cards[left]
+            left += 1
+        else:
+            dima += cards[right]
+            right -= 1
+print(sereja, dima)
