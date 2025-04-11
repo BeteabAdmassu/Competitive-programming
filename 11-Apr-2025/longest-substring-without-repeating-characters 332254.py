6dffda9df969386d94df9a98b7a0c5ddc737fcbb
Beteab Admassu <82194935+BeteabAdmassu@users.noreply.github.com>
2025-04-11 22:26:00 +0300

    Created 'longest-substring-without-repeating-characters' by A2SV Hub

---
+++ 11-Apr-2025/longest-substring-without-repeating-characters 332254.py
@@ -0,0 +1,27 @@
+# Problem: longest-substring-without-repeating-characters - https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+
+        longest_substring = 0 
+
+        left, right = 0, 0
+        char_set = set()
+      
+
+        while right < len(s):
+
+            if s[right] not in char_set:
+                char_set.add(s[right])
+                right += 1
+                longest_substring = max(longest_substring, right - left)
+
+            else:
+                char_set.discard(s[left])
+                left += 1
+    
+        return longest_substring
+
+
+
+        
