b6d1f10121dc091d92073a0e07ce0c303f6ac8ef
Beteab Admassu <82194935+BeteabAdmassu@users.noreply.github.com>
2025-05-23 21:53:04 +0300

    Created 'Copy List with Random Pointer' by A2SV Hub

---
+++ 23-May-2025/Copy List with Random Pointer 359257.py
@@ -0,0 +1,40 @@
+# Problem: Copy List with Random Pointer - https://leetcode.com/problems/copy-list-with-random-pointer/
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head:
+            return None
+        
+        current = head
+        while current:
+            new_node = Node(current.val)
+            new_node.next = current.next
+            current.next = new_node
+            current = new_node.next
+        
+        current = head
+        while current:
+            if current.random:
+                current.next.random = current.random.next
+            current = current.next.next
+        
+        current = head
+        new_head = head.next
+        while current:
+            temp = current.next
+            current.next = temp.next
+            if temp.next:
+                temp.next = temp.next.next
+            current = current.next
+        
+        return new_head
+        
