472f995495a3a7d4a7ebfcf186d7ea3c85455d7e
Beteab Admassu <82194935+BeteabAdmassu@users.noreply.github.com>
2025-09-22 21:57:48 +0300

    Created 'Maximum Gap' by A2SV Hub

---
+++ 22-Sep-2025/Maximum Gap 395098.py
@@ -0,0 +1,12 @@
+# Problem: Maximum Gap - https://leetcode.com/problems/maximum-gap/description/
+
+class Solution:
+    def maximumGap(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n < 2:
+            return 0
+        nums.sort()
+        max_diff = 0
+        for i in range(n-1):
+            max_diff = max(max_diff, (nums[i+1] - nums[i]))
+        return max_diff
